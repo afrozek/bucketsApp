8d13490800a276c1326983fac656bb28916267f2
Afroze Khan <afrozek95@gmail.com>
2020-03-22 12:13:05 -0400

    new nav design, pulling nav links from component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  navItems: { displayText: string; iconPath: string; anchorLink: string; }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.initNav();
+  }
+
+  initNav() {
+    this.navItems = [
+      {
+        displayText: "Dashboard",
+        iconPath: "/assets/images/dashboard-icon.svg",
+        anchorLink: ""
+      },
+      {
+        displayText: "Reports",
+        iconPath: "/assets/images/reports-icon.svg",
+        anchorLink: ""
+      },
+      {
+        displayText: "Transactions",
+        iconPath: "/assets/images/transactions-icon.svg",
+        anchorLink: ""
+      }
+    ]
   }
 
 }
